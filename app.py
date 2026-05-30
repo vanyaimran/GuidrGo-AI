95980ff000105b79bfd43b781b5a8e4a1e5cfa84
vanyaimran <f2024-0921@bnu.edu.pk>
2026-05-30 16:39:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,23 @@ travel_type = st.selectbox(
 )
 
 if st.button("Generate Travel Report"):
+
     st.success(f"Generating report for {city}")
+
+    st.subheader("🌦 Weather Analysis")
+    st.write("Condition: Sunny")
+    st.write("Temperature: 25°C")
+    st.write("Risk Level: Low")
+
+    st.subheader("🏨 Recommended Hotel")
+    st.write("Grand Taj Hotel")
+    st.write("Rating: 4.4/5")
+
+    st.subheader("🚗 Traffic Analysis")
+    st.write("Traffic Risk: Medium")
+
+    st.subheader("🗺 Suggested Itinerary")
+
+    st.write("Day 1: Mall Road")
+    st.write("Day 2: Patriata")
+    st.write("Day 3: Kashmir Point")
